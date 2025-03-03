7863bebc062290f0a54a556c021ced8885b3ebac
Hoài Tiên05 <bohonuochthihoaitien@gmail.com>
2025-03-04 08:22:36 +1345

    done blade

--- resources/views/masterShopee.blade.php
+++ resources/views/masterShopee.blade.php
@@ -30,13 +30,6 @@
 	
 	@include('footershopee')
 	
-
-  
-    <script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.scrollUp.min.js"></script>
-	<script src="js/price-range.js"></script>
-    <script src="js/jquery.prettyPhoto.js"></script>
-    <script src="js/main.js"></script>
+    @include('criptShopee')
 </body>
 </html>
